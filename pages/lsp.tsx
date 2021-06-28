66cfb5600a1412e538098c42c1c82469e301bec4
Gamaranto <fra@hey.com>
2021-06-28 12:14:28 +0200

    add lsp page

---
+++ pages/lsp.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Layout } from "../components";
+import { BaseButton } from "../components/Button";
+
+const LSPPage: React.FC = () => {
+  // can use all components and stuff here
+  return (
+    <Layout title="LSP Interface">
+      <BaseButton>Click me</BaseButton>
+    </Layout>
+  );
+};
+
+export default LSPPage;
